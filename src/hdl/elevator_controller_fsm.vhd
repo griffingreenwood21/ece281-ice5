be1489d4742ffaf895d308a8cdc1d75c73c57ee5
griffingreenwood21 <141801624+griffingreenwood21@users.noreply.github.com>
2024-04-02 09:06:12 -0600

    completed fsm with register process, next state logic, and output logic

--- src/hdl/elevator_controller_fsm.vhd
+++ src/hdl/elevator_controller_fsm.vhd
@@ -87,7 +87,7 @@ architecture Behavioral of elevator_controller_fsm is
     -- variable type can take on. Now you can assign a signal as 
     -- "sm_floor" the same way you'd assign a signal as std_logic
 	-- how would you modify this to go up to 15 floors?
-	type sm_floor is (s_floor1, s_floor2, s_floor3, s_floor4);
+	type sm_floor is (s_floor1, s_floor2, s_floor3, s_floor4);	
 	
 	-- Here you create variables that can take on the values defined above. Neat!	
 	signal f_Q, f_Q_next: sm_floor;
@@ -96,20 +96,20 @@ begin
 
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	-- Next State Logic
-    f_Q_next <= <state> when (<condition>) else -- going up
-                ...
-                ...
-                ... -- going down
-                ...
-                ... else
-                ...; -- default case
+    f_Q_next <= s_floor2 when (f_Q = s_floor1 and i_up_down = '1') else -- going up
+                s_floor3 when (f_Q = s_floor2 and i_up_down = '1') else
+                s_floor4 when (f_Q = s_floor3 and i_up_down = '1') else
+                s_floor3 when (f_Q = s_floor4 and i_up_down = '0') else -- going down
+                s_floor2 when (f_Q = s_floor3 and i_up_down = '0') else
+                s_floor1 when (f_Q = s_floor2 and i_up_down = '0') else
+                f_Q; -- default case
   
 	-- Output logic
     with f_Q select
-        o_floor <= <value> when s_floor1,
-                ...
-                ...
-                <value> when others; -- default is floor 2
+        o_floor <= "0001" when s_floor1,
+                   "0011" when s_floor3,
+                   "0100" when s_floor4,
+                   "0010" when others; -- default is floor 2
 	
 	-------------------------------------------------------------------------------------------------------
 	
@@ -117,10 +117,17 @@ begin
 	-- State memory ------------
 	register_proc : process (i_clk)
     begin
-         -- synchronous reset
-        
-        -- if elevator is enabled, advance floors
-        -- if not enabled, stay at current floor
+        -- totally synchronous
+        if rising_edge(i_clk) then
+            -- synchronous reset
+            if i_reset = '1' then
+                f_Q <= s_floor2;
+            -- if elevator is enabled, advance floors
+            -- if not enabled, stay at current floor
+            elsif (i_stop = '0') then
+                f_Q <= f_Q_next;
+            end if; -- otherwise, stay at same floor
+        end if;
     
 	end process register_proc;	
 	
